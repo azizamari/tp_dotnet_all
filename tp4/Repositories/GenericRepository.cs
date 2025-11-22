c2be1da4834961e17b6692be00427d27325aadce
Aziz Amari <azizamari2003@gmail.com>
2025-11-22 10:20:00 +0100

    Implement GenericRepository for Entity Framework Core integration

---
+++ tp4/Repositories/GenericRepository.cs
@@ -0,0 +1,51 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace tp4.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(DbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public T GetById(object id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public void Insert(T obj)
+        {
+            _dbSet.Add(obj);
+        }
+
+        public void Update(T obj)
+        {
+            _dbSet.Attach(obj);
+            _context.Entry(obj).State = EntityState.Modified;
+        }
+
+        public void Delete(object id)
+        {
+            T existing = _dbSet.Find(id);
+            if (existing != null)
+            {
+                _dbSet.Remove(existing);
+            }
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
