5dff22974e4ec180c13d6c6ca03bb9304a245442
Aziz Amari <azizamari2003@gmail.com>
2025-11-22 10:10:00 +0100

    Introduce IGenericRepository interface for data abstraction

---
+++ tp4/Repositories/IGenericRepository.cs
@@ -0,0 +1,12 @@
+namespace tp4.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(object id);
+        void Insert(T obj);
+        void Update(T obj);
+        void Delete(object id);
+        void Save();
+    }
+}
